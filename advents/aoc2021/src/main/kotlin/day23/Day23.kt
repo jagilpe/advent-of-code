dc9a4d3c5ae2ec8fe28d1aedcbac5fb1c5acce21
Javier Gil Pereda <javier@gilpereda.com>
2021-12-29 15:44:09 +0100

    Another attempt for day 23

--- advents/aoc2021/src/main/kotlin/day23/Day23.kt
+++ advents/aoc2021/src/main/kotlin/day23/Day23.kt
@@ -1,17 +1,174 @@
+@file:OptIn(ExperimentalStdlibApi::class)
+
 package com.gilpereda.adventsofcode.adventsofcode2021.day23
 
 import com.gilpereda.adventsofcode.adventsofcode2021.Executable
 
 
-typealias Cave = List<MutableList<out Cell>>
+val part1: Executable = { input ->
+    val game = parse(input)
 
-val part1: (Cave) -> Int = { TODO() }
+    winner(listOf(game))
+    TODO()
+}
 
 val part2: Executable = { TODO() }
+
+val played = mutableListOf<List<List<Cell>>>()
+val winner: DeepRecursiveFunction<List<Game>, List<Game>> = DeepRecursiveFunction { games ->
+    println(games.size)
+    val (finished, notFinished) = games.partition { it.finished }
+    played.addAll(games.map { it.cellMap })
+    val next = notFinished.flatMap { it.nextMoves() }.filter { it.cellMap !in played }
+    if (notFinished.isEmpty()) {
+        finished
+    } else {
+        finished + callRecursive(next)
+    }
+}
+
+data class Game(val cellMap: List<List<Cell>>, val moves: List<Move> = emptyList()) {
+    override fun toString(): String =
+        cellMap.joinToString("\n") { it.joinToString("") }
+
+    private fun Room.accepts(amphipod: Amphipod): Boolean =
+        free && amphipod == accepted
+                && cellMap.flatten().filter { it is Room && it.accepted == amphipod }
+            .all { it.free || it.amphipod == amphipod }
+
+    fun canMoveTo(from: Point, to: Point): Boolean {
+        val origin = cell(from)
+        val amphipod = origin.amphipod
+        return if (amphipod != null && (origin !is Room || !origin.revisited )) {
+            when (val destination = cell(to)) {
+                is Hall -> destination.free && origin is Room
+                is Room -> destination.accepts(amphipod)
+                else -> false
+            }
+        } else {
+            false
+        }
+    }
+
+    fun canMoveThrough(through: Point): Boolean = cell(through).free
+
+    private fun makeMove(move: Move): Game {
+        val (amphipod, from, to) = move
+        val newCellMap = cellMap.mapIndexed { y, row ->
+            row.mapIndexed { x, cell ->
+                when {
+                    x == from.x && y == from.y -> cell.empty()
+                    x == to.x && y == to.y -> cell.fill(amphipod)
+                    else -> cell
+                }
+            }
+        }
+        return copy(cellMap = newCellMap, moves = moves + move )
+    }
+
+    fun nextMoves(): Sequence<Game> =
+        cellMap.flatMapIndexed { y, row ->
+            row.flatMapIndexed { x, cell ->
+                val amphipod = cell.amphipod
+                if (amphipod != null) {
+                    nextMovesFrom(amphipod, Point(x, y))
+                } else {
+                    emptyList()
+                }
+            }
+        }.asSequence()
+            .map { makeMove(it) }
+
+    private fun nextMovesFrom(amphipod: Amphipod, point: Point): List<Move> {
+        tailrec fun go(visited: Set<Point>, neighbours: List<Point>, consumed: Int, acc: List<Move>): List<Move> {
+            val moves = neighbours.filter { it !in visited && canMoveTo(point, it) }
+                .map { Move(amphipod, point, it, consumed + amphipod.consume) }
+            val newNeighbours = neighbours
+                .flatMap { it.neighbours }
+                .filter { it !in visited && canMoveThrough(it) }
+            return if (newNeighbours.isNotEmpty()) {
+                go(visited + neighbours, newNeighbours, amphipod.consume + consumed, acc + moves)
+            } else {
+                acc + moves
+            }
+        }
+
+        val neighbours = point.neighbours.filter { canMoveThrough(it) }
+        return go(setOf(point), neighbours, 0, emptyList())
+    }
+
+
+    val finished: Boolean
+        get() = cellMap.flatten().filterIsInstance<Room>().all { it.amphipod == it.accepted }
+
+    private fun cell(point: Point): Cell = cellMap[point.y][point.x]
+}
+
+val Point.neighbours: List<Point>
+    get() = listOf(
+        Point(x - 1, y),
+        Point(x, y - 1),
+        Point(x + 1, y),
+        Point(x, y + 1),
+    )
+
+data class Move(val amphipod: Amphipod, val from: Point, val to: Point, val consumed: Int)
+
+fun parse(input: Sequence<String>): Game =
+    input.mapIndexed { y, row ->
+        row.mapIndexed { x, char ->
+            when (char) {
+                '#' -> Wall
+                '.' -> parseEmptyCell(x, y)
+                else -> parseOccupiedCell(char, x, y)
+            }
+        }
+    }.toList().let(::Game)
+
+fun parseEmptyCell(x: Int, y: Int): Cell =
+    when (y) {
+        1 -> when (x) {
+            3, 5, 7, 9 -> Door
+            else -> Hall()
+        }
+        else -> when (x) {
+            3 -> Room(Amphipod.AMBER)
+            5 -> Room(Amphipod.BRONZE)
+            7 -> Room(Amphipod.COPPER)
+            9 -> Room(Amphipod.DESERT)
+            else -> throw Exception("illegal room point $x, $y")
+        }
+    }
+
+fun parseOccupiedCell(char: Char, x: Int, y: Int): Cell {
+    val amphipod = when (char) {
+        'A' -> Amphipod.AMBER
+        'B' -> Amphipod.BRONZE
+        'C' -> Amphipod.COPPER
+        'D' -> Amphipod.DESERT
+        else -> throw IllegalArgumentException()
+    }
+    return when (y) {
+        1 -> when (x) {
+            3, 5, 7, 9 -> throw IllegalArgumentException("Doors can not be")
+            else -> Hall(amphipod)
+        }
+        else -> when (x) {
+            3 -> Room(Amphipod.AMBER, amphipod)
+            5 -> Room(Amphipod.BRONZE, amphipod)
+            7 -> Room(Amphipod.COPPER, amphipod)
+            9 -> Room(Amphipod.DESERT, amphipod)
+            else -> throw Exception("illegal room point $x, $y")
+        }
+    }
+}
+
 
 sealed interface Cell {
     val free: Boolean
     val amphipod: Amphipod?
+    fun empty(): Cell
+    fun fill(amphipod: Amphipod?): Cell
 }
 
 enum class Amphipod(val consume: Int) {
@@ -24,35 +181,38 @@ enum class Amphipod(val consume: Int) {
 object Wall : Cell {
     override val free: Boolean = false
     override val amphipod: Amphipod? = null
+    override fun empty(): Cell = throw UnsupportedOperationException()
+    override fun fill(amphipod: Amphipod?): Cell = throw UnsupportedOperationException()
+
+    override fun toString(): String = "#"
 }
 
 data class Hall(override val amphipod: Amphipod? = null) : Cell {
     override val free: Boolean = amphipod == null
+    override fun empty(): Cell = copy(amphipod = null)
+    override fun fill(amphipod: Amphipod?): Cell = copy(amphipod = amphipod)
+
+    override fun toString(): String = if (amphipod != null) "${amphipod.name.first()}" else "."
 }
 
 object Door : Cell {
     override val free: Boolean = true
     override val amphipod: Amphipod? = null
+    override fun empty(): Cell = throw UnsupportedOperationException()
+    override fun fill(amphipod: Amphipod?): Cell = throw UnsupportedOperationException()
+
+    override fun toString(): String = "x"
 }
 
-data class Room(override val amphipod: Amphipod? = null, private val accepted: Amphipod) : Cell {
+data class Room(val accepted: Amphipod, override val amphipod: Amphipod? = null) : Cell {
+    var revisited: Boolean = false
     override val free: Boolean = amphipod == null
+    override fun empty(): Cell = copy(amphipod = null)
+    override fun fill(amphipod: Amphipod?): Cell = copy(amphipod = amphipod).apply { revisited = true }
+
+    override fun toString(): String = if (amphipod != null) "${amphipod.name.first()}" else "o"
 }
 
-data class Move(val cave: Cave, val cost: Int)
-
-data class Point(val x: Int, val y: Int)
-
-fun Cave.move(from: Point, to: Point): Move {
-    val source = this[from.y][from.x]
-    val dest = this[to.y][to.x]
-    val amphipod = source.amphipod
-    if (amphipod != null) {
-        TODO()
-    } else {
-        throw Exception("Source cell $source is empty")
-    }
+data class Point(val x: Int, val y: Int) {
+    override fun toString(): String = "($x, $y)"
 }
-
-val Cave.nextMoves: List<Move>
-    get() = TODO()
--- advents/aoc2021/src/test/kotlin/day23/Day23Test.kt
+++ advents/aoc2021/src/test/kotlin/day23/Day23Test.kt
@@ -5,19 +5,139 @@ import com.gilpereda.adventsofcode.adventsofcode2021.Executable
 import com.gilpereda.adventsofcode.adventsofcode2021.day23.Amphipod.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.Arguments.of
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
-class Day23Test {
+class Day23Test : BaseTest() {
+
+    override val example: String = """
+        #############
+        #...........#
+        ###B#C#B#D###
+        ###A#D#C#A###
+        #############
+        """.trimIndent()
+
+    override val result1: String = ""
+
+    override val result2: String
+        get() = TODO("Not yet implemented")
+    override val input: String = "/day23/input.txt"
+
+    override val run1: Executable = part1
+
+    override val run2: Executable = part2
 
     @Test
-    fun `should work with the example 1`() {
-        val example = listOf(
-            mutableListOf(Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall),
-            mutableListOf(Wall, Hall(), Hall(), Door, Hall(), Door, Hall(), Door, Hall(), Door, Hall(), Hall(), Wall),
-            mutableListOf(Wall, Wall, Wall, Room(BRONZE, AMBER), Wall, Room(COPPER, BRONZE), Wall, Room(BRONZE, COPPER), Wall, Room(DESERT, DESERT), Wall, Wall, Wall),
-            mutableListOf(Wall, Wall, Wall, Room(AMBER, AMBER), Wall, Room(DESERT, BRONZE), Wall, Room(COPPER, COPPER), Wall, Room(AMBER, DESERT), Wall, Wall, Wall),
-            mutableListOf(Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall),
+    fun `should parse the input`() {
+        val expected = Game(listOf(
+            listOf(Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall),
+            listOf(Wall, Hall(), Hall(), Door, Hall(), Door, Hall(), Door, Hall(), Door, Hall(), Hall(), Wall),
+            listOf(Wall, Wall, Wall, Room(AMBER, BRONZE), Wall, Room(BRONZE, COPPER), Wall, Room(COPPER, BRONZE), Wall, Room(DESERT, DESERT), Wall, Wall, Wall),
+            listOf(Wall, Wall, Wall, Room(AMBER, AMBER), Wall, Room(BRONZE, DESERT), Wall, Room(COPPER, COPPER), Wall, Room(DESERT, AMBER), Wall, Wall, Wall),
+            listOf(Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall),
+        ))
+        assertThat(parse(example.splitToSequence("\n"))).isEqualTo(expected)
+    }
+
+    @ParameterizedTest
+    @MethodSource("finished")
+    fun `should check if the game is finished`(game: Game, finished: Boolean) {
+        assertThat(game.finished).isEqualTo(finished)
+    }
+
+    @ParameterizedTest
+    @MethodSource("movementsTo")
+    fun `should get if a movement is possible`(m: String, game: Game, origin: Point, destination: Point, possible: Boolean) {
+        assertThat(game.canMoveTo(origin, destination)).isEqualTo(possible)
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("movementsThrough")
+    fun `should get if a movement through a cell is possible`(m: String, game: Game, cell: Point, possible: Boolean) {
+        assertThat(game.canMoveThrough(cell)).isEqualTo(possible)
+    }
+
+    @Test
+    fun `should not continue if no movement is possible`() {
+        val deadGame = initialGame.cellMap.map { row -> row.map { cell ->
+            when (cell) {
+                is Room -> cell.apply { revisited = true }
+                else -> cell
+            }
+        }}.let(::Game)
+
+        assertThat(deadGame.nextMoves().toList()).isEmpty()
+    }
+
+    companion object {
+        @JvmStatic
+        fun movementsTo(): Stream<Arguments> = Stream.of(
+            of("From wall", initialGame, Point(2, 0), Point(4, 1), false),
+            of("From room to free hall", initialGame, Point(3, 2), Point(4, 1), true),
+            of("From hall valid cell", otherGame, Point(10, 1), Point(7, 3), true),
+            of("From hall invalid cell", otherGame, Point(6, 1), Point(7, 3), false),
+            of("From hall to partially occupied room invalid", otherGame, Point(6, 1), Point(5, 2), false),
+            of("From hall to partially occupied room valid", otherGame, Point(2, 1), Point(3, 2), true),
+            of("To a room's door", otherGame, Point(2, 1), Point(3, 1), false),
+            of("Between hall points", otherGame, Point(2, 1), Point(4, 1), false),
         )
-        assertThat(part1(example)).isEqualTo(12521)
+
+        @JvmStatic
+        fun movementsThrough(): Stream<Arguments> = Stream.of(
+            of("Through wall", initialGame, Point(2, 0), false),
+            of("Through empty room cell", otherGame, Point(3, 2), true),
+            of("Through occupied room cell", otherGame, Point(9, 2), false),
+            of("Through room door", otherGame, Point(5, 1), true),
+            of("Through free hall cell", otherGame, Point(4, 1), true),
+            of("Through occupied hall cell", otherGame, Point(6, 1), false),
+        )
+
+        @JvmStatic
+        fun finished(): Stream<Arguments> = Stream.of(
+            of(initialGame, false),
+            of(otherGame, false),
+            of(finishedGame, true),
+            of(finishedGamePart2, true),
+        )
     }
 
 }
+
+private val initialGame: Game = """
+            #############
+            #...........#
+            ###B#C#B#D###
+            ###A#D#C#A###
+            #############
+        """.trimIndent().splitToSequence("\n").let(::parse)
+
+private val otherGame: Game = """
+            #############
+            #.A...B...C.#
+            ###.#.#.#D###
+            ###A#D#.#A###
+            #############
+        """.trimIndent().splitToSequence("\n").let(::parse)
+
+private val finishedGame: Game = """
+            #############
+            #...........#
+            ###A#B#C#D###
+            ###A#B#C#D###
+            #############
+        """.trimIndent().splitToSequence("\n").let(::parse)
+
+private val finishedGamePart2: Game = """
+            #############
+            #...........#
+            ###A#B#C#D###
+            ###A#B#C#D###
+            ###A#B#C#D###
+            ###A#B#C#D###
+            #############
+        """.trimIndent().splitToSequence("\n").let(::parse)
