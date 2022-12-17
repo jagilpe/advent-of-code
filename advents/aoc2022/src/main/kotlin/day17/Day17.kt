df210eb6dc4f42e897376449fec89a566406e8e4
Javier Gil Pereda <javier@gilpereda.com>
2022-12-17 13:34:14 +0100

    Day17 part 1

--- advents/aoc2022/src/main/kotlin/day17/Day17.kt
+++ advents/aoc2022/src/main/kotlin/day17/Day17.kt
@@ -1,5 +1,273 @@
 package com.gilpereda.aoc2022.day17
 
-fun firstTask(input: Sequence<String>): String = TODO()
+fun firstTask(input: Sequence<String>): String {
+    val movements: List<Movement> = input.first().parsed()
+
+    val last = generateSequence(Game(movements = movements)) {
+        it.next()
+    }
+        .takeWhile { it.rocksPositions.size < 2023 }.last()
+    return last.towerHeight.toString()
+}
 
 fun secondTask(input: Sequence<String>): String = TODO()
+
+fun String.parsed(): List<Movement> =
+    map {
+        when (it) {
+            '<' -> Left
+            '>' -> Right
+            else -> throw Exception("Unknown direction")
+        }
+    }
+
+fun List<RockPosition>.dump(header: String? = null, lines: Int? = null): String {
+    val pointsInMovement = filter { !it.stopped }.flatMap { it.points }
+    val stoppedPoints = filter { it.stopped }.flatMap { it.points }
+    val highestY = maxOfOrNull(RockPosition::nextY) ?: 4
+
+    val toLine = maxOf(lines?.let { highestY - lines } ?: 0, ) - 1
+    return (highestY downTo toLine)
+        .joinToString(separator = "\n", prefix = header?.let { "$it\n" } ?: "") { y ->
+            if (y > -1) {
+                (0..6).joinToString(separator = "", prefix = "|", postfix = "|") { x ->
+                    when {
+                        Point(x, y) in pointsInMovement -> "@"
+                        Point(x, y) in stoppedPoints -> "#"
+                        else -> "."
+                    }
+                }
+            } else {
+                "\n+-------+\n"
+            }
+        }
+            .also(::println)
+}
+
+data class Game(
+    val movements: List<Movement>,
+    val nextMovements: List<Movement> = movements,
+    val rocksOrder: List<Rock> = Rock.orderedRocks,
+    val nextRocks: List<Rock> = rocksOrder,
+    val rocksPositions: List<RockPosition> = emptyList(),
+    val boardWidth: Int = 7,
+) {
+//    override fun toString(): String = """Game(
+//        |nextMovements: $nextMovements
+//        |nextRocks: $nextRocks
+//        |rocksPositions: $rocksPositions
+//        |)
+//    """.trimMargin()
+
+    fun dump(lines: Int = 20): String {
+        return rocksPositions.dump(lines = lines).also { println("\n\n") }
+    }
+
+    override fun toString(): String = rocksPositions.dump()
+
+    val towerHeight: Int by lazy { firstFreeLine }
+
+    private val firstFreeLine: Int by lazy {
+        rocksPositions.maxOfOrNull(RockPosition::nextY) ?: 0
+    }
+
+    private val blockingElements: Set<RockPosition> by lazy {
+//        (0..6).mapNotNull(::highestBlockingForColumn).toSet()
+        rocksPositions.filter { it.stopped }.toSet()
+    }
+
+    private fun highestBlockingForColumn(column: Int): RockPosition? =
+        rocksPositions.filter(RockPosition::stopped)
+            .map { it to it.highestYAtX(column) }
+            .filter { it.second >= 0 }
+            .maxByOrNull { it.second }?.first
+
+    private fun RockPosition.highestYAtX(x: Int): Int =
+        point.y - 1 + rock.heightAt(x - point.x)
+
+    fun next(): Game =
+        if (canDrop) {
+            val movedRock = rocksPositions.last()
+                .push(nextMovements.first()).drop()
+            copy(
+                nextMovements = nextMovements.drop(1).ifEmpty { movements },
+                rocksPositions = rocksPositions.dropLast(1) + movedRock,
+            )
+        } else {
+            copy(
+                nextMovements = nextMovements,
+                nextRocks = nextRocks.drop(1).ifEmpty { rocksOrder },
+                rocksPositions = rocksPositions + nextRocks.first().pos(firstFreeLine + 3)
+            )
+        }
+
+    private fun RockPosition.push(movement: Movement): RockPosition =
+        when (movement) {
+            is Left -> copy(point = point.copy(x = point.x - 1))
+            is Right -> copy(point = point.copy(x = point.x + 1))
+        }
+            .let { if (it.valid) it else this }
+
+
+    private fun RockPosition.drop(): RockPosition =
+        copy(point = point.copy(y = maxOf(point.y - 1, 0)), stopped = point.y == 0)
+            .let { if (it.valid) it else copy(stopped = true) }
+
+    private val RockPosition.valid: Boolean
+        get() = (point.x in 0..(boardWidth - rock.width)) && blockingElements.none { it.overlaps(this) }
+
+    private val canDrop: Boolean
+        get() = !(rocksPositions.lastOrNull()?.stopped ?: true)
+}
+
+data class RockPosition(
+    val rock: Rock,
+    val point: Point,
+    val stopped: Boolean = false,
+) {
+    val nextY: Int = point.y + rock.height
+
+    fun stop(): RockPosition = copy(stopped = true)
+
+    fun overlaps(other: RockPosition): Boolean =
+        points.intersect(other.points).isNotEmpty()
+
+    val points: Set<Point>
+        get() = rock.points.map { (x, y) -> Point(point.x + x, point.y + y) }.toSet()
+}
+
+sealed interface Movement
+
+object Left : Movement {
+    override fun toString(): String = "<"
+}
+
+object Right : Movement {
+    override fun toString(): String = ">"
+}
+
+data class Point(
+    val x: Int,
+    val y: Int,
+)
+
+interface Rock {
+    val points: List<Point>
+    val height: Int
+    val width: Int
+
+    fun heightAt(x: Int): Int
+
+    companion object {
+        val orderedRocks: List<Rock> = listOf(
+            HorizontalLine,
+            Plus,
+            InvertedL,
+            VerticalLine,
+            Square
+        )
+    }
+
+    fun pos(x: Int, y: Int): RockPosition = RockPosition(this, Point(x, y))
+
+    fun pos(y: Int): RockPosition = RockPosition(this, Point(2, y))
+}
+
+object HorizontalLine : Rock {
+    override val points: List<Point> = listOf(
+        Point(0, 0),
+        Point(1, 0),
+        Point(2, 0),
+        Point(3, 0),
+    )
+
+    override val height: Int = 1
+    override val width: Int = 4
+    override fun heightAt(x: Int): Int =
+        if (x in 0..3) 1 else Int.MIN_VALUE
+
+    override fun toString(): String = "_"
+}
+
+object Plus : Rock {
+    override val points: List<Point> = listOf(
+        Point(1, 0),
+        Point(0, 1),
+        Point(1, 1),
+        Point(2, 1),
+        Point(1, 2),
+    )
+
+    override val height: Int = 3
+    override val width: Int = 3
+    override fun heightAt(x: Int): Int =
+        when (x) {
+            0 -> 2
+            1 -> 3
+            2 -> 2
+            else -> Int.MIN_VALUE
+        }
+
+    override fun toString(): String = "+"
+}
+
+object InvertedL : Rock {
+    override val points: List<Point> = listOf(
+        Point(0, 0),
+        Point(1, 0),
+        Point(2, 0),
+        Point(2, 1),
+        Point(2, 2),
+    )
+
+    override val height: Int = 3
+    override val width: Int = 3
+    override fun heightAt(x: Int): Int =
+        when (x) {
+            0 -> 1
+            1 -> 2
+            2 -> 3
+            else -> Int.MIN_VALUE
+        }
+
+    override fun toString(): String = "L"
+}
+
+object VerticalLine : Rock {
+    override val points: List<Point> = listOf(
+        Point(0, 0),
+        Point(0, 1),
+        Point(0, 2),
+        Point(0, 3),
+    )
+
+    override val height: Int = 4
+    override val width: Int = 1
+    override fun heightAt(x: Int): Int =
+        when (x) {
+            0 -> 4
+            else -> Int.MIN_VALUE
+        }
+
+    override fun toString(): String = "|"
+}
+
+object Square : Rock {
+    override val points: List<Point> = listOf(
+        Point(0, 0),
+        Point(1, 0),
+        Point(0, 1),
+        Point(1, 1),
+    )
+
+    override val height: Int = 2
+    override val width: Int = 2
+    override fun heightAt(x: Int): Int =
+        when (x) {
+            0 -> 2
+            1 -> 2
+            else -> Int.MIN_VALUE
+        }
+
+    override fun toString(): String = "*"
+}
--- advents/aoc2022/src/test/kotlin/day17/Day17Test.kt
+++ advents/aoc2022/src/test/kotlin/day17/Day17Test.kt
@@ -4,11 +4,19 @@ import com.gilpereda.aoc2022.BaseTest
 import com.gilpereda.aoc2022.Executable
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.Arguments.of
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
+/**
+ * 3064 -> too low
+ */
 class Day17Test : BaseTest() {
-    override val example: String = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
+    override val example: String = Day17Test.example
 
-    override val result1: String = ""
+    override val result1: String = "3068"
 
     override val result2: String
         get() = TODO()
@@ -18,5 +26,279 @@ class Day17Test : BaseTest() {
     override val run1: Executable = ::firstTask
 
     override val run2: Executable = ::secondTask
+
+    @Test
+    fun `should parse the input`() {
+        val parsed = ">>><<><>>".parsed()
+        val expected = listOf(
+            Right, Right, Right, Left, Left, Right, Left, Right, Right
+        )
+
+        assertThat(parsed).isEqualTo(expected)
+    }
+
+    @ParameterizedTest
+    @MethodSource("movements")
+    fun `should move the rocks`(after: Int, expected: Game) {
+        val actual = generateSequence(initialState) { it.next() }.take(after + 1).last()
+
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @ParameterizedTest
+    @MethodSource("rocksInitialPosition")
+    fun `should position the rocks correctly`(nextRock: Rock, expected: Point, previous: List<RockPosition>) {
+        val game = initialState.copy(
+            nextRocks =  listOf(nextRock),
+            rocksPositions = previous
+        )
+
+        assertThat(game.next().rocksPositions.first()).isEqualTo(nextRock.pos(expected.x, expected.y))
+    }
+
+    @ParameterizedTest
+    @MethodSource("rocksMovements")
+    fun `should move the rocks correctly`(initialPos: RockPosition, movement: Movement, expected: Point) {
+        val game = initialState.copy(
+            nextMovements = listOf(movement),
+            nextRocks =  emptyList(),
+            rocksPositions = listOf(initialPos)
+        )
+
+        assertThat(game.next().rocksPositions.first().point).isEqualTo(expected)
+    }
+
+    @ParameterizedTest
+    @MethodSource("movementOrchestration")
+    fun `should orchestrate the movements`(initial: Game, expected: Game) {
+        val actual = initial.next()
+        initial.rocksPositions.dump("initial")
+        expected.rocksPositions.dump("expected")
+        actual.rocksPositions.dump("actual")
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @ParameterizedTest
+    @MethodSource("positionOverlaps")
+    fun `should detect an overlap`(one: RockPosition, other: RockPosition, expected: Boolean) {
+        assertThat(one.overlaps(other)).isEqualTo(expected)
+    }
+
+    @Test
+    fun testSomething() {
+        generateSequence(initialState) {
+            it.next()
+        }.onEach { it.rocksPositions.dump() }.take(100).count()
+
+    }
+
+
+    companion object {
+        const val example: String = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
+        private val initialState = Game(movements = example.parsed())
+
+        @JvmStatic
+        fun positionOverlaps(): Stream<Arguments> = Stream.of(
+            of(HorizontalLine.pos(0, 0), VerticalLine.pos(0,0), true),
+            of(HorizontalLine.pos(0, 0), VerticalLine.pos(1,0), true),
+            of(HorizontalLine.pos(0, 0), VerticalLine.pos(2,0), true),
+            of(HorizontalLine.pos(0, 0), VerticalLine.pos(3,0), true),
+            of(HorizontalLine.pos(0, 0), VerticalLine.pos(4,0), false),
+            of(HorizontalLine.pos(0, 0), VerticalLine.pos(5,0), false),
+            of(HorizontalLine.pos(0, 0), VerticalLine.pos(6,0), false),
+        )
+
+            @JvmStatic
+        fun movements(): Stream<Arguments> = with(initialState) {
+            Stream.of(
+                of(
+                    1, copy(
+                        nextMovements = nextMovements,
+                        nextRocks = nextRocks.drop(1),
+                        rocksPositions = listOf(HorizontalLine.pos(2, 3))
+                    )
+                ),
+                of(
+                    2, copy(
+                        nextMovements = nextMovements.drop(1),
+                        nextRocks = nextRocks.drop(1),
+                        rocksPositions = listOf(HorizontalLine.pos(3, 2))
+                    )
+                ),
+                of(
+                    3, copy(
+                        nextMovements = nextMovements.drop(2),
+                        nextRocks = nextRocks.drop(1),
+                        rocksPositions = listOf(HorizontalLine.pos(3, 1))
+                    )
+                ),
+                of(
+                    4, copy(
+                        nextMovements = nextMovements.drop(3),
+                        nextRocks = nextRocks.drop(1),
+                        rocksPositions = listOf(HorizontalLine.pos(3, 0))
+                    )
+                ),
+                of(
+                    5, copy(
+                        nextMovements = nextMovements.drop(4),
+                        nextRocks = nextRocks.drop(1),
+                        rocksPositions = listOf(HorizontalLine.pos(2, 0).stop())
+                    )
+                ),
+            )
+        }
+
+        @JvmStatic
+        fun movementOrchestration(): Stream<Arguments> = Stream.of(
+            testCase {
+                val initialPositions = listOf(HorizontalLine.pos(0, 0).stop())
+                val initial = copy(
+                    nextRocks = listOf(Plus),
+                    rocksPositions = initialPositions
+                )
+                val expected = copy(
+                    nextRocks = rocksOrder,
+                    rocksPositions = initialPositions + Plus.pos(4)
+                )
+                initial to expected
+            },
+            testCase {
+                val initial = copy(
+                    nextMovements = listOf(Right),
+                    rocksPositions = listOf(HorizontalLine.pos(0, 2))
+                )
+                val expected = copy(
+                    nextMovements = movements,
+                    rocksPositions = listOf(HorizontalLine.pos(1, 1))
+                )
+                initial to expected
+            },
+            testCase {
+                val otherRocks = listOf(HorizontalLine.pos(3, 1).stop())
+                val initial = copy(
+                    nextMovements = listOf(Right),
+                    rocksPositions = otherRocks + Plus.pos(0, 1)
+                )
+                val expected = copy(
+                    nextMovements = movements,
+                    rocksPositions =  otherRocks + Plus.pos(1, 1).stop()
+                )
+                initial to expected
+            },
+            testCase {
+                val otherRocks = listOf(HorizontalLine.pos(2, 0).stop())
+                val initial = copy(
+                    nextMovements = listOf(Left),
+                    rocksPositions = otherRocks + Plus.pos(2, 1)
+                )
+                val expected = copy(
+                    nextMovements = movements,
+                    rocksPositions =  otherRocks + Plus.pos(1, 1).stop()
+                )
+                initial to expected
+            },
+        )
+
+        @JvmStatic
+        fun rocksInitialPosition(): Stream<Arguments> = Stream.of(
+            of(HorizontalLine, Point(2, 3), emptyList<RockPosition>()),
+            of(HorizontalLine, Point(2, 4), listOf(HorizontalLine.pos(0, 0).stop())),
+            of(HorizontalLine, Point(2, 5), listOf(HorizontalLine.pos(3, 1).stop())),
+            of(HorizontalLine, Point(2, 6), listOf(HorizontalLine.pos(2, 2).stop())),
+            of(Plus, Point(2, 3), emptyList<RockPosition>()),
+            of(Plus, Point(2, 5), listOf(HorizontalLine.pos(0, 1).stop())),
+            of(InvertedL, Point(2, 3), emptyList<RockPosition>()),
+            of(InvertedL, Point(2, 5), listOf(HorizontalLine.pos(0, 1).stop())),
+            of(VerticalLine, Point(2, 3), emptyList<RockPosition>()),
+            of(VerticalLine, Point(2, 5), listOf(HorizontalLine.pos(0, 1).stop())),
+            of(Square, Point(2, 3), emptyList<RockPosition>()),
+            of(Square, Point(2, 5), listOf(HorizontalLine.pos(0, 1).stop())),
+
+            of(HorizontalLine, Point(2, 6), listOf(Plus.pos(0, 0).stop())),
+            of(HorizontalLine, Point(2, 8), listOf(Plus.pos(0, 2).stop())),
+
+            of(HorizontalLine, Point(2, 6), listOf(InvertedL.pos(0, 0).stop())),
+            of(HorizontalLine, Point(2, 8), listOf(InvertedL.pos(0, 2).stop())),
+
+            of(HorizontalLine, Point(2, 7), listOf(VerticalLine.pos(0, 0).stop())),
+            of(HorizontalLine, Point(2, 9), listOf(VerticalLine.pos(0, 2).stop())),
+
+            of(HorizontalLine, Point(2, 5), listOf(Square.pos(0, 0).stop())),
+            of(HorizontalLine, Point(2, 7), listOf(Square.pos(0, 2).stop())),
+        )
+
+        @JvmStatic
+        fun moveRockHorizontal(): Stream<Arguments> = Stream.of(
+            of(HorizontalLine.pos(0, 0), Right, Point(1, 0)),
+            of(HorizontalLine.pos(1, 0), Right, Point(2, 0)),
+            of(HorizontalLine.pos(2, 0), Right, Point(3, 0)),
+            of(HorizontalLine.pos(3, 0), Right, Point(3, 0)),
+            of(HorizontalLine.pos(0, 0), Left, Point(0, 0)),
+            of(HorizontalLine.pos(1, 0), Left, Point(0, 0)),
+            of(HorizontalLine.pos(2, 0), Left, Point(1, 0)),
+            of(HorizontalLine.pos(3, 0), Left, Point(2, 0)),
+
+            of(Plus.pos(0, 0), Right, Point(1, 0)),
+            of(Plus.pos(1, 0), Right, Point(2, 0)),
+            of(Plus.pos(2, 0), Right, Point(3, 0)),
+            of(Plus.pos(3, 0), Right, Point(4, 0)),
+            of(Plus.pos(4, 0), Right, Point(4, 0)),
+            of(Plus.pos(0, 0), Left, Point(0, 0)),
+            of(Plus.pos(1, 0), Left, Point(0, 0)),
+            of(Plus.pos(2, 0), Left, Point(1, 0)),
+            of(Plus.pos(3, 0), Left, Point(2, 0)),
+            of(Plus.pos(4, 0), Left, Point(3, 0)),
+
+            of(InvertedL.pos(0, 0), Right, Point(1, 0)),
+            of(InvertedL.pos(1, 0), Right, Point(2, 0)),
+            of(InvertedL.pos(2, 0), Right, Point(3, 0)),
+            of(InvertedL.pos(3, 0), Right, Point(4, 0)),
+            of(InvertedL.pos(4, 0), Right, Point(5, 0)),
+            of(InvertedL.pos(5, 0), Right, Point(5, 0)),
+            of(InvertedL.pos(0, 0), Left, Point(0, 0)),
+            of(InvertedL.pos(1, 0), Left, Point(0, 0)),
+            of(InvertedL.pos(2, 0), Left, Point(1, 0)),
+            of(InvertedL.pos(3, 0), Left, Point(2, 0)),
+            of(InvertedL.pos(4, 0), Left, Point(3, 0)),
+            of(InvertedL.pos(5, 0), Left, Point(4, 0)),
+
+            of(VerticalLine.pos(0, 0), Right, Point(1, 0)),
+            of(VerticalLine.pos(1, 0), Right, Point(2, 0)),
+            of(VerticalLine.pos(2, 0), Right, Point(3, 0)),
+            of(VerticalLine.pos(3, 0), Right, Point(4, 0)),
+            of(VerticalLine.pos(4, 0), Right, Point(5, 0)),
+            of(VerticalLine.pos(5, 0), Right, Point(6, 0)),
+            of(VerticalLine.pos(6, 0), Right, Point(6, 0)),
+            of(VerticalLine.pos(0, 0), Left, Point(0, 0)),
+            of(VerticalLine.pos(1, 0), Left, Point(0, 0)),
+            of(VerticalLine.pos(2, 0), Left, Point(1, 0)),
+            of(VerticalLine.pos(3, 0), Left, Point(2, 0)),
+            of(VerticalLine.pos(4, 0), Left, Point(3, 0)),
+            of(VerticalLine.pos(5, 0), Left, Point(4, 0)),
+            of(VerticalLine.pos(6, 0), Left, Point(5, 0)),
+
+            of(Square.pos(0, 0), Right, Point(1, 0)),
+            of(Square.pos(1, 0), Right, Point(2, 0)),
+            of(Square.pos(2, 0), Right, Point(3, 0)),
+            of(Square.pos(3, 0), Right, Point(4, 0)),
+            of(Square.pos(4, 0), Right, Point(5, 0)),
+            of(Square.pos(5, 0), Right, Point(5, 0)),
+            of(Square.pos(0, 0), Left, Point(0, 0)),
+            of(Square.pos(1, 0), Left, Point(0, 0)),
+            of(Square.pos(2, 0), Left, Point(1, 0)),
+            of(Square.pos(3, 0), Left, Point(2, 0)),
+            of(Square.pos(4, 0), Left, Point(3, 0)),
+            of(Square.pos(5, 0), Left, Point(4, 0)),
+        )
+
+        private fun testCase(
+            initial: Game.() -> Pair<Game, Game>,
+        ): Arguments =
+            initial(initialState).let { (initial, expected) ->
+                of(initial, expected)
+            }
+    }
+
 
 }
