3c8e02680f49157fb8b61a115152262976c35eff
Javier Gil Pereda <javier@gilpereda.com>
2022-12-20 06:38:38 +0100

    Day 20 WIP

--- advents/aoc2022/src/main/kotlin/day20/Day20.kt
+++ advents/aoc2022/src/main/kotlin/day20/Day20.kt
@@ -1,5 +1,28 @@
 package com.gilpereda.aoc2022.day20
 
-fun firstTask(input: Sequence<String>): String = TODO()
+fun firstTask(input: Sequence<String>): String {
+    val list = input.parsed()
+
+    list.foldIndexed(list) { index, acc, num ->
+        when {
+            num == 0 -> acc
+            num > 0 -> {
+                val newIndex = (index + num) % acc.size
+                when {
+                    newIndex == index -> acc
+                    newIndex < index -> acc.subList(0, newIndex) + num + acc.subList(newIndex, index) + acc.subList(index + 1, acc.size - 1)
+                    else -> acc.subList(0, index) + acc.subList(index, newIndex) + num + acc.subList(newIndex + 1, acc.size - 1)
+                }
+            }
+            else -> {
+                val newIndex = (index - num)
+            }
+        }
+    }
+}
 
 fun secondTask(input: Sequence<String>): String = TODO()
+
+fun Sequence<String>.parsed(): List<Int> =
+    map { it.toInt() }.toList()
+
--- advents/aoc2022/src/test/kotlin/day20/Day20Test.kt
+++ advents/aoc2022/src/test/kotlin/day20/Day20Test.kt
@@ -6,13 +6,20 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class Day20Test : BaseTest() {
-    override val example: String = TODO()
+    override val example: String = """1
+2
+-3
+3
+-2
+0
+4"""
 
-    override val result1: String = TODO()
+    override val result1: String = "3"
 
-    override val result2: String = TODO()
+    override val result2: String
+        get() = TODO()
 
-    override val input: String = TODO()
+    override val input: String = "/day20/input"
 
     override val run1: Executable = ::firstTask
 
