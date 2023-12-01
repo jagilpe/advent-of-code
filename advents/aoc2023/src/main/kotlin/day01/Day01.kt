24f400abe17d8df1c39b191c5f610925b4a177d6
Javier Gil Pereda <javier@gilpereda.com>
2023-12-01 06:36:01 +0100

    Day 1

--- advents/aoc2023/src/main/kotlin/day01/Day01.kt
+++ advents/aoc2023/src/main/kotlin/day01/Day01.kt
@@ -1,7 +1,83 @@
 package com.gilpereda.aoc2022.day01
 
+import java.lang.Exception
+
 fun firstTask(input: Sequence<String>): String =
-    TODO()
+    input.map(::findNumber).sum().toString()
 
 fun secondTask(input: Sequence<String>): String =
-    TODO()
+    input.map { line ->
+        "${line.firstDigit2}${line.lastDigit2}".toInt()
+    }.sum().toString()
+
+
+fun findNumber(line: String): Int =
+    "${line.firstDigit}${line.lastDigit}".toInt()
+
+val String.firstDigit: Char
+    get() = first { it.isDigit() }
+
+val String.lastDigit: Char
+    get() = reversed().first { it.isDigit() }
+
+val String.firstDigit2: Int
+    get() {
+        fun go(rest: String): Int =
+            when (val first = wordToDigitStart(rest)) {
+                null -> go(rest.drop(1))
+                else -> first
+            }
+        return go(this)
+    }
+
+val String.lastDigit2: Int
+    get() {
+        fun go(rest: String): Int =
+            try {
+                when (val last = wordToDigitEnd(rest)) {
+                    null -> go(rest.dropLast(1))
+                    else -> last
+                }
+            } catch (ex: Exception) {
+                println(this)
+                println(rest)
+                throw ex
+            }
+        return go(this)
+    }
+
+fun wordToDigitStart(line: String): Int? {
+    val lower = line.lowercase()
+    return when {
+        lower.first().isDigit() -> lower.first().digitToInt()
+        lower.startsWith("zero") -> 0
+        lower.startsWith("one") -> 1
+        lower.startsWith("two") -> 2
+        lower.startsWith("three") -> 3
+        lower.startsWith("four") -> 4
+        lower.startsWith("five") -> 5
+        lower.startsWith("six") -> 6
+        lower.startsWith("seven") -> 7
+        lower.startsWith("eight") -> 8
+        lower.startsWith("nine") -> 9
+        else -> null
+    }
+}
+
+fun wordToDigitEnd(line: String): Int? {
+    val lower = line.lowercase()
+    return when {
+        lower.last().isDigit() -> lower.last().digitToInt()
+        lower.endsWith("zero") -> 0
+        lower.endsWith("one") -> 1
+        lower.endsWith("two") -> 2
+        lower.endsWith("three") -> 3
+        lower.endsWith("four") -> 4
+        lower.endsWith("five") -> 5
+        lower.endsWith("six") -> 6
+        lower.endsWith("seven") -> 7
+        lower.endsWith("eight") -> 8
+        lower.endsWith("nine") -> 9
+        else -> null
+    }
+}
--- advents/aoc2023/src/test/kotlin/day01/Day01Test.kt
+++ advents/aoc2023/src/test/kotlin/day01/Day01Test.kt
@@ -4,16 +4,28 @@ import com.gilpereda.aoc2022.BaseTest
 import com.gilpereda.aoc2022.Executable
 
 class Day01Test : BaseTest() {
-    override val example: String
-        get() = TODO()
+    override val example: String = """
+        1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet
+    """.trimIndent()
 
-    override val result1: String
-        get() = TODO()
+    override val example2: String = """
+        two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen
+    """.trimIndent()
 
-    override val result2: String
-        get() = TODO()
+    override val result1: String = "142"
 
-    override val input: String = "/day04/input"
+    override val result2: String = "281"
+
+    override val input: String = "/day01/input"
 
     override val run1: Executable = ::firstTask
 
