6f687a0856517e355587c094dc71890fd5f97e87
Javier Gil Pereda <javier@gilpereda.com>
2023-12-05 07:34:35 +0100

    Days 5

--- advents/aoc2023/src/test/kotlin/day05/Day05Test.kt
+++ advents/aoc2023/src/test/kotlin/day05/Day05Test.kt
@@ -4,16 +4,47 @@ import com.gilpereda.aoc2022.BaseTest
 import com.gilpereda.aoc2022.Executable
 
 class Day05Test : BaseTest() {
-    override val example: String
-        get() = TODO()
+    override val example: String = """
+        seeds: 79 14 55 13
 
-    override val result1: String
-        get() = TODO()
+        seed-to-soil map:
+        50 98 2
+        52 50 48
 
-    override val result2: String
-        get() = TODO()
+        soil-to-fertilizer map:
+        0 15 37
+        37 52 2
+        39 0 15
 
-    override val input: String = "/day04/input"
+        fertilizer-to-water map:
+        49 53 8
+        0 11 42
+        42 0 7
+        57 7 4
+
+        water-to-light map:
+        88 18 7
+        18 25 70
+
+        light-to-temperature map:
+        45 77 23
+        81 45 19
+        68 64 13
+
+        temperature-to-humidity map:
+        0 69 1
+        1 0 69
+
+        humidity-to-location map:
+        60 56 37
+        56 93 4
+    """.trimIndent()
+
+    override val result1: String = "35"
+
+    override val result2: String = "46"
+
+    override val input: String = "/day05/input"
 
     override val run1: Executable = ::firstTask
 
