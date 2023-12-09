1d714ed20ec3b76c99f3ce051042702a814c0c9e
Javier Gil Pereda <javier@gilpereda.com>
2023-12-09 07:06:23 +0100

    Day 9

--- advents/aoc2023/src/test/kotlin/day09/Day09Test.kt
+++ advents/aoc2023/src/test/kotlin/day09/Day09Test.kt
@@ -4,14 +4,15 @@ import com.gilpereda.aoc2022.BaseTest
 import com.gilpereda.aoc2022.Executable
 
 class Day09Test : BaseTest() {
-    override val example: String
-        get() = TODO()
+    override val example: String = """
+        0 3 6 9 12 15
+        1 3 6 10 15 21
+        10 13 16 21 30 45
+    """.trimIndent()
 
-    override val result1: String
-        get() = TODO()
+    override val result1: String = "114"
 
-    override val result2: String
-        get() = TODO()
+    override val result2: String = "2"
 
     override val input: String = "/day09/input"
 
