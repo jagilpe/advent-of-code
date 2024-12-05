e97d9b19ef46e69084567c281a947c65ff945de9
Javier Gil Pereda <javier-gil.pereda@idealo.de>
2024-12-05 07:19:00 +0100

    Day 5 resolution

--- advents/aoc2024/src/main/kotlin/day05/Day05.kt
+++ advents/aoc2024/src/main/kotlin/day05/Day05.kt
@@ -1,7 +1,107 @@
 package com.gilpereda.aoc2024.day05
 
-fun firstTask(input: Sequence<String>): String =
-    TODO()
+fun firstTask(input: Sequence<String>): String {
+    val (rules, updates, _) = input.parsed()
+    return updates
+        .filter { it.isValid(rules) }
+        .sumOf { it.findMiddle() }
+        .toString()
+}
 
-fun secondTask(input: Sequence<String>): String =
-    TODO()
+private fun Sequence<String>.parsed(): Triple<Rules, List<Update>, Boolean> =
+    fold(Triple(Rules(), emptyList<Update>(), false)) { (rules, updates, spaceFound), next ->
+        if (!spaceFound) {
+            if (next.isBlank()) {
+                Triple(rules, updates, true)
+            } else {
+                Triple(rules.addRule(next), updates, spaceFound)
+            }
+        } else {
+            Triple(rules, updates + Update.from(next), spaceFound)
+        }
+    }
+
+fun secondTask(input: Sequence<String>): String {
+    val (rules, updates, _) = input.parsed()
+    return updates
+        .filter { !it.isValid(rules) }
+        .map {
+            var corrected = it
+            while (!corrected.isValid(rules)) {
+                corrected = corrected.corrected(rules)
+            }
+            corrected
+        }.sumOf {
+            it.findMiddle()
+        }.toString()
+}
+
+class Rules(
+    val rules: MutableMap<Int, MutableList<Int>> = mutableMapOf(),
+) {
+    fun addRule(rule: String): Rules =
+        apply {
+            val (previous, following) = rule.split("|")
+            rules.computeIfAbsent(following.toInt()) { mutableListOf() }.add(previous.toInt())
+        }
+}
+
+class Update(
+    val pages: List<Int>,
+) {
+    fun isValid(rules: Rules): Boolean {
+        tailrec fun go(
+            valid: Boolean,
+            pages: List<Int>,
+        ): Boolean =
+            if (pages.isEmpty() || !valid) {
+                valid
+            } else {
+                val next = pages.first()
+                val rest = pages.drop(1)
+                go(next.isValid(rest, rules), pages.drop(1))
+            }
+        return go(true, pages)
+    }
+
+    fun corrected(rules: Rules): Update {
+        tailrec fun go(
+            acc: List<Int>,
+            pages: List<Int>,
+        ): Update =
+            if (pages.isEmpty()) {
+                Update(acc)
+            } else {
+                val next = pages.first()
+                val rest = pages.drop(1)
+                val wrongPages = next.wrongPages(rest, rules)
+                go(acc + wrongPages + next, rest - wrongPages)
+            }
+        return go(emptyList(), pages)
+    }
+
+    private fun Int.wrongPages(
+        rest: List<Int>,
+        rules: Rules,
+    ): Set<Int> =
+        rules.rules[this]?.let { pages ->
+            rest.filter { follower -> follower in pages }.toSet()
+        } ?: emptySet()
+
+    private fun Int.isValid(
+        following: List<Int>,
+        rules: Rules,
+    ): Boolean =
+        rules.rules[this]?.let { pages ->
+            following.all { follower -> follower !in pages }
+        } ?: true
+
+    fun findMiddle(): Int {
+        val middle = if (pages.size % 2 == 0) (pages.size / 2) - 1 else pages.size / 2
+        return pages[middle]
+    }
+
+    companion object {
+        fun from(line: String): Update = Update(line.split(",").map { it.toInt() })
+    }
+}
--- advents/aoc2024/src/test/kotlin/day05/Day05Test.kt
+++ advents/aoc2024/src/test/kotlin/day05/Day05Test.kt
@@ -4,23 +4,45 @@ import com.gilpereda.aoc2024.BaseTest
 import com.gilpereda.aoc2024.Executable
 
 class Day05Test : BaseTest() {
-    override val example: String
-        get() = TODO()
+    override val example: String =
+        """
+        47|53
+        97|13
+        97|61
+        97|47
+        75|29
+        61|13
+        75|53
+        29|13
+        97|29
+        53|29
+        61|53
+        97|53
+        61|29
+        47|13
+        75|47
+        97|75
+        47|61
+        75|61
+        47|29
+        75|13
+        53|13
 
-    override val example2: String
-        get() = TODO()
+        97,13,75,29,47
+        75,97,47,61,53
+        75,47,61,53,29
+        97,61,53,29,13
+        75,29,13
+        61,13,29
+        """.trimIndent()
 
-    override val resultExample1: String
-        get() = TODO()
+    override val resultExample1: String = "143"
 
-    override val resultReal1: String
-        get() = TODO()
+    override val resultReal1: String = "3608"
 
-    override val resultExample2: String
-        get() = TODO()
+    override val resultExample2: String = "123"
 
-    override val resultReal2: String
-        get() = TODO()
+    override val resultReal2: String = "4922"
 
     override val input: String = "/day05/input"
 
