8efdf6fd8106e29cff3c0e224f4b6c37b37622b7
Javier Gil Pereda <javier-gil.pereda@idealo.de>
2024-12-11 08:18:06 +0100

    Day 11 part two

--- advents/aoc2024/src/main/kotlin/day11/Day11.kt
+++ advents/aoc2024/src/main/kotlin/day11/Day11.kt
@@ -1,36 +1,61 @@
 package com.gilpereda.aoc2024.day11
 
-fun firstTask(input: Sequence<String>): String = task(input, 25)
+typealias Cache = MutableMap<Long, Game.Element>
 
-private fun List<Long>.nextGen(): List<Long> = flatMap { it.nextGen() }
+fun firstTask(input: Sequence<String>): String = task(input, 25)
 
 fun secondTask(input: Sequence<String>): String = task(input, 75)
 
 private fun task(
     input: Sequence<String>,
     repetitions: Int,
-): String {
-    val firstGen = input.first().split(" ").map { it.toLong() }
-
-    return generateSequence(1 to firstGen) { (iter: Int, gen: List<Long>) ->
-        println("Iteration: $iter")
-        (iter + 1) to gen.nextGen()
-    }.take(repetitions + 1)
-        .last()
-        .second
-        .size
+): String =
+    Game(input.first().split(" ").map { it.toLong() })
+        .elementsAfter(repetitions)
         .toString()
-}
 
-private fun Long.nextGen(): List<Long> =
-    when {
-        this == 0L -> listOf(1L)
-        ("$this".length) % 2 == 0 ->
-            "$this".let {
-                val center = (it.length / 2)
-                val first = it.substring(0..<center).toLongOrNull()
-                val second = it.substring(center..<it.length).toLongOrNull()
-                listOfNotNull(first, second)
+class Game(
+    private val firstGen: List<Long>,
+) {
+    private val cache: Cache = mutableMapOf()
+
+    fun elementsAfter(generations: Int): Long =
+        firstGen.sumOf { value ->
+            cache.computeIfAbsent(value) { Element(it) }.childrenForGeneration(generations)
+        }
+
+    inner class Element(
+        value: Long,
+    ) {
+        private val children: List<Element> by lazy {
+            when {
+                value == 0L -> listOf(cache.computeIfAbsent(1) { Element(it) })
+                "$value".length % 2 == 0 -> {
+                    val center = ("$value".length / 2)
+                    val first = "$value".substring(0..<center).toLong()
+                    val second = "$value".substring(center..<"$value".length).toLong()
+                    listOf(
+                        cache.computeIfAbsent(first) { Element(it) },
+                        cache.computeIfAbsent(second) { Element(it) },
+                    )
+                }
+                else -> listOf(cache.computeIfAbsent(value * 2024) { Element(it) })
             }
-        else -> listOf(this * 2024L)
+        }
+
+        private val generationToChildren: MutableMap<Int, Long> = mutableMapOf()
+
+        fun childrenForGeneration(generation: Int): Long =
+            if (generation == 0) {
+                1
+            } else {
+                when (val cached = generationToChildren[generation]) {
+                    null ->
+                        children
+                            .sumOf { it.childrenForGeneration(generation - 1) }
+                            .also { generationToChildren[generation] = it }
+                    else -> cached
+                }
+            }
     }
+}
--- advents/aoc2024/src/test/kotlin/day11/Day11Test.kt
+++ advents/aoc2024/src/test/kotlin/day11/Day11Test.kt
@@ -6,16 +6,13 @@ import com.gilpereda.aoc2024.Executable
 class Day11Test : BaseTest() {
     override val example: String = "125 17"
 
-    override val example2: String
-        get() = TODO()
-
     override val resultExample1: String = "55312"
 
     override val resultReal1: String = "231278"
 
-    override val resultExample2: String = ""
+    override val resultExample2: String = "65601038650482"
 
-    override val resultReal2: String = ""
+    override val resultReal2: String = "274229228071551"
 
     override val input: String = "/day11/input"
 
