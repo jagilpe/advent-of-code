503782120df262ae0e064d7b14435de218b01be8
Javier Gil Pereda <javier-gil.pereda@idealo.de>
2024-12-11 06:27:41 +0100

    Day 11 part one

--- advents/aoc2024/src/main/kotlin/day11/Day11.kt
+++ advents/aoc2024/src/main/kotlin/day11/Day11.kt
@@ -1,7 +1,36 @@
 package com.gilpereda.aoc2024.day11
 
-fun firstTask(input: Sequence<String>): String =
-    TODO()
+fun firstTask(input: Sequence<String>): String = task(input, 25)
 
-fun secondTask(input: Sequence<String>): String =
-    TODO()
+private fun List<Long>.nextGen(): List<Long> = flatMap { it.nextGen() }
+
+fun secondTask(input: Sequence<String>): String = task(input, 75)
+
+private fun task(
+    input: Sequence<String>,
+    repetitions: Int,
+): String {
+    val firstGen = input.first().split(" ").map { it.toLong() }
+
+    return generateSequence(1 to firstGen) { (iter: Int, gen: List<Long>) ->
+        println("Iteration: $iter")
+        (iter + 1) to gen.nextGen()
+    }.take(repetitions + 1)
+        .last()
+        .second
+        .size
+        .toString()
+}
+
+private fun Long.nextGen(): List<Long> =
+    when {
+        this == 0L -> listOf(1L)
+        ("$this".length) % 2 == 0 ->
+            "$this".let {
+                val center = (it.length / 2)
+                val first = it.substring(0..<center).toLongOrNull()
+                val second = it.substring(center..<it.length).toLongOrNull()
+                listOfNotNull(first, second)
+            }
+        else -> listOf(this * 2024L)
+    }
--- advents/aoc2024/src/test/kotlin/day11/Day11Test.kt
+++ advents/aoc2024/src/test/kotlin/day11/Day11Test.kt
@@ -4,23 +4,18 @@ import com.gilpereda.aoc2024.BaseTest
 import com.gilpereda.aoc2024.Executable
 
 class Day11Test : BaseTest() {
-    override val example: String
-        get() = TODO()
+    override val example: String = "125 17"
 
     override val example2: String
         get() = TODO()
 
-    override val resultExample1: String
-        get() = TODO()
+    override val resultExample1: String = "55312"
 
-    override val resultReal1: String
-        get() = TODO()
+    override val resultReal1: String = "231278"
 
-    override val resultExample2: String
-        get() = TODO()
+    override val resultExample2: String = ""
 
-    override val resultReal2: String
-        get() = TODO()
+    override val resultReal2: String = ""
 
     override val input: String = "/day11/input"
 
