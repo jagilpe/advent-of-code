fbcd53b970792be9bdd59ff19e08501a7fe86f33
Javier Gil Pereda <javier-gil.pereda@idealo.de>
2024-12-17 19:27:57 +0100

    Days 17

--- advents/aoc2024/src/main/kotlin/day17/Day17.kt
+++ advents/aoc2024/src/main/kotlin/day17/Day17.kt
@@ -1,7 +1,198 @@
 package com.gilpereda.aoc2024.day17
 
-fun firstTask(input: Sequence<String>): String =
-    TODO()
+import kotlin.math.pow
 
-fun secondTask(input: Sequence<String>): String =
-    TODO()
+typealias Program = Map<Int, Operation>
+
+fun firstTask(input: Sequence<String>): String {
+    val (regA, regB, regC, _, programStr) = input.toList()
+
+    val computer =
+        Computer(
+            regA.removePrefix("Register A: ").toLong(),
+            regB.removePrefix("Register B: ").toLong(),
+            regC.removePrefix("Register C: ").toLong(),
+        )
+    val program = programStr.parsed()
+    return computer.runProgram(program).output.joinToString(",")
+}
+
+fun secondTask(input: Sequence<String>): String {
+    val (_, regBStr, regCStr, _, programStr) = input.toList()
+
+    val start = System.currentTimeMillis()
+    val program = programStr.parsed()
+    println(program)
+    val expected = programStr.removePrefix("Program: ")
+    var regA = -1L
+    val regB = regBStr.removePrefix("Register B: ").toLong()
+    val regC = regCStr.removePrefix("Register C: ").toLong()
+
+    do {
+        regA++
+        val result = Computer(regA, regB, regC).runProgram(program).outputString
+        if (expected.endsWith(result)) {
+            println("$regA $result")
+            if (expected == result) break
+            regA =
+                if (regA > 6175748441) {
+                    (regA * 8) - 1_000_000
+                } else {
+                    (regA * 8)
+                }
+        }
+    } while (true)
+    return regA.toString()
+}
+
+fun String.parsed(): Program =
+    removePrefix("Program: ")
+        .split(",")
+        .chunked(2)
+        .mapIndexed(Operation::from)
+        .toMap()
+
+class Computer(
+    var registerA: Long,
+    var registerB: Long,
+    var registerC: Long,
+) {
+    var pointer: Int = 0
+    var output: MutableList<Int> = mutableListOf()
+
+    override fun toString(): String =
+        "register A: $registerA register B: $registerB register C: $registerC pointer: $pointer output: $output"
+
+    val outputString: String
+        get() = output.joinToString(",")
+
+    fun runProgram(program: Program): Computer {
+        var operation = program[pointer]
+        while (operation != null) {
+            operation.run(this)
+            operation = program[pointer]
+        }
+        return this
+    }
+
+    fun next() {
+        pointer += 2
+    }
+
+    fun jumpTo(instruction: Int) {
+        pointer = instruction
+    }
+
+    fun comboOperand(operand: Int): Long =
+        when (operand) {
+            0, 1, 2, 3 -> operand.toLong()
+            4 -> registerA
+            5 -> registerB
+            6 -> registerC
+            else -> throw IllegalArgumentException("Invalid operand $operand")
+        }
+}
+
+sealed interface Operation {
+    val operand: Int
+
+    fun run(computer: Computer)
+
+    companion object {
+        fun from(
+            index: Int,
+            list: List<String>,
+        ): Pair<Int, Operation> =
+            Pair(
+                index * 2,
+                when (list.first()) {
+                    "0" -> Adv(list[1].toInt())
+                    "1" -> Bxl(list[1].toInt())
+                    "2" -> Bst(list[1].toInt())
+                    "3" -> Jnz(list[1].toInt())
+                    "4" -> Bxc
+                    "5" -> Out(list[1].toInt())
+                    "6" -> Bdv(list[1].toInt())
+                    "7" -> Cdv(list[1].toInt())
+                    else -> throw IllegalArgumentException()
+                },
+            )
+    }
+}
+
+data class Adv(
+    override val operand: Int,
+) : Operation {
+    override fun run(computer: Computer) {
+        computer.registerA /= 2.0.pow(computer.comboOperand(operand)).toInt()
+        computer.next()
+    }
+}
+
+data class Bxl(
+    override val operand: Int,
+) : Operation {
+    override fun run(computer: Computer) {
+        computer.registerB = computer.registerB xor operand.toLong()
+        computer.next()
+    }
+}
+
+data class Bst(
+    override val operand: Int,
+) : Operation {
+    override fun run(computer: Computer) {
+        computer.registerB = computer.comboOperand(operand) % 8
+        computer.next()
+    }
+}
+
+data class Jnz(
+    override val operand: Int,
+) : Operation {
+    override fun run(computer: Computer) {
+        if (computer.registerA != 0L) {
+            computer.jumpTo(operand.toInt())
+        } else {
+            computer.next()
+        }
+    }
+}
+
+data object Bxc : Operation {
+    override val operand: Int = 0
+
+    override fun run(computer: Computer) {
+        computer.registerB = computer.registerB xor computer.registerC
+        computer.next()
+    }
+}
+
+data class Out(
+    override val operand: Int,
+) : Operation {
+    override fun run(computer: Computer) {
+        computer.output.add((computer.comboOperand(operand) % 8).toInt())
+        computer.next()
+    }
+}
+
+data class Bdv(
+    override val operand: Int,
+) : Operation {
+    override fun run(computer: Computer) {
+        computer.registerB = computer.registerA / 2.0.pow(computer.comboOperand(operand)).toInt()
+        computer.next()
+    }
+}
+
+data class Cdv(
+    override val operand: Int,
+) : Operation {
+    override fun run(computer: Computer) {
+        computer.registerC = computer.registerA / 2.0.pow(computer.comboOperand(operand)).toInt()
+        computer.next()
+    }
+}
+
+private fun Double.pow(value: Long): Long = toDouble().pow(value.toDouble()).toLong()
--- advents/aoc2024/src/test/kotlin/day17/Day17Test.kt
+++ advents/aoc2024/src/test/kotlin/day17/Day17Test.kt
@@ -2,29 +2,122 @@ package com.gilpereda.aoc2024.day17
 
 import com.gilpereda.aoc2024.BaseTest
 import com.gilpereda.aoc2024.Executable
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 class Day17Test : BaseTest() {
-    override val example: String
-        get() = TODO()
+    override val example: String =
+        """
+        Register A: 729
+        Register B: 0
+        Register C: 0
 
-    override val example2: String
-        get() = TODO()
+        Program: 0,1,5,4,3,0
+        """.trimIndent()
 
-    override val resultExample1: String
-        get() = TODO()
+    override val example2: String =
+        """
+        Register A: 2024
+        Register B: 0
+        Register C: 0
 
-    override val resultReal1: String
-        get() = TODO()
+        Program: 0,3,5,4,3,0
+        """.trimIndent()
 
-    override val resultExample2: String
-        get() = TODO()
+    override val resultExample1: String = "4,6,3,5,6,3,5,2,1,0"
 
-    override val resultReal2: String
-        get() = TODO()
+    override val resultReal1: String = "7,4,2,0,5,0,5,3,7"
+
+    override val resultExample2: String = "117440"
+
+    override val resultReal2: String = "202991746427434"
 
     override val input: String = "/day17/input"
 
     override val run1: Executable = ::firstTask
 
     override val run2: Executable = ::secondTask
+
+    @ParameterizedTest
+    @MethodSource
+    fun `should make the calculations`(
+        customizer: Computer.() -> Unit,
+        programString: String,
+        actualExtractor: (Computer) -> Any,
+        expected: Any,
+    ) {
+        val program = programString.parsed()
+        val computer =
+            Computer(0, 0, 0)
+                .apply(customizer)
+                .runProgram(program)
+        val actual = actualExtractor(computer)
+
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    private fun `should make the calculations`(): Stream<Arguments> =
+        Stream.of(
+            testCase(
+                { it.registerC = 9 },
+                "2,6",
+                { it.registerB },
+                1L,
+            ),
+            testCase(
+                { it.registerA = 10 },
+                "5,0,5,1,5,4",
+                { it.outputString },
+                "0,1,2",
+            ),
+            testCase(
+                { it.registerA = 2024 },
+                "0,1,5,4,3,0",
+                { it.outputString to it.registerA },
+                "4,2,5,6,7,7,7,7,3,1,0" to 0L,
+            ),
+            testCase(
+                { it.registerB = 29 },
+                "1,7",
+                { it.registerB },
+                26L,
+            ),
+            testCase(
+                {
+                    it.registerB = 2024
+                    it.registerC = 43690
+                },
+                "4,0",
+                { it.registerB },
+                44354L,
+            ),
+            testCase(
+                { it.registerA = 2024 },
+                "0,0",
+                { it.registerA },
+                2024L,
+            ),
+            testCase(
+                { it.registerA = 2024 },
+                "0,1",
+                { it.registerA },
+                1012L,
+            ),
+            testCase(
+                { it.registerA = 2024 },
+                "0,2",
+                { it.registerA },
+                506L,
+            ),
+        )
+
+    private fun testCase(
+        computer: (Computer) -> Unit,
+        programString: String,
+        actual: (Computer) -> Any,
+        expected: Any,
+    ): Arguments = Arguments.of(computer, programString, actual, expected)
 }
